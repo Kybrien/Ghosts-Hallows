e2395f62e9cfd14d0aaa0a9315bb8019c683e2c9
Hroycode <hroy@gaming.tech>
2024-03-04 03:58:25 +0100

    Ui+ gameLogic

--- Assets/Scripts/GoalCondition.cs
+++ Assets/Scripts/GoalCondition.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GoalCondition : MonoBehaviour
 {
@@ -9,12 +11,18 @@ public class GoalCondition : MonoBehaviour
     public int scoreJ1 = 0; // Score actuel
     public int scoreJ2 = 0; // Score actuel
     private Rigidbody rb;
-    public TMPro.TMP_Text playerOverlayScore;
+    public TMPro.TMP_Text J1Score;
+    public TMPro.TMP_Text J2Score;
+    public TMPro.TMP_Text Winner;
 
     void Start()
     {
         // Récupère le Rigidbody attaché à la balle
         rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        CheckWinner();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -27,13 +35,13 @@ public class GoalCondition : MonoBehaviour
             {
                 scoreJ1 += scoreValue;
                 Debug.Log("But marqué ! Score J1 : " + scoreJ1);
-                UpdateScoreDisplay();
+                J1Score.text = "" + scoreJ1;
             }
             else if (gameObject.CompareTag("GoalP2"))
             {
                 scoreJ2 += scoreValue;
                 Debug.Log("But marqué ! Score J2 : " + scoreJ2);
-                UpdateScoreDisplay();
+                J2Score.text = "" + scoreJ2;
             }
 
 
@@ -41,8 +49,37 @@ public class GoalCondition : MonoBehaviour
             collision.gameObject.transform.position = respawnPoint.position;
         }
     }
-    void UpdateScoreDisplay()
+    void CheckWinner()
     {
-        playerOverlayScore.text = scoreJ1 + " - " + scoreJ2 ;
+        if (scoreJ1 >= 5)
+        {
+            Debug.Log("Player 1 Wins!");
+            Winner.text = "" + "Player 1 Wins!";
+            ResetGame();
+        }
+        else if (scoreJ2 >= 5)
+        {
+            Debug.Log("Player 2 Wins!");
+            Winner.text = "" + "Player 2 Wins!";
+            ResetGame();
+        }
     }
+
+    void ResetGame()
+    {
+        // Reset scores, ball position, and any other necessary game state
+        scoreJ1 = 0;
+        scoreJ2 = 0;
+        J1Score.text = "0";
+        J2Score.text = "0";
+        Invoke("ResetScene", 2);
+
+    }
+    void ResetScene()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+    }
+
+
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,24 +3,58 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public TMPro.TMP_Text TimerText; // Assign in the inspector
+    public TMPro.TMP_Text TimerText;
+    public TMPro.TMP_Text StartingGame;
+
+
     private float timeRemaining = 300; // 5 minutes in seconds
+    private float countdownTime = 3; // 3 seconds countdown
+    private bool isCountdownActive = true;
+
+    public void RestartCountdown()
+    {
+        timeRemaining = 300; // Reset game timer to 5 minutes
+        countdownTime = 3; // Reset countdown timer to 3 seconds
+        isCountdownActive = true; // Reactivate countdown
+        StartingGame.text = ""; // Clear any starting game text
+        TimerText.text = ""; // Clear the main timer text
+    }
 
     void Update()
     {
-        if (timeRemaining > 0)
+        if (isCountdownActive)
         {
-            Debug.Log("Time's still going!");
+            if (countdownTime > 0)
+            {
+                countdownTime -= Time.deltaTime;
+                StartingGame.text = Mathf.CeilToInt(countdownTime).ToString();
+            }
+            else
+            {
+                isCountdownActive = false;
+                StartingGame.text = "La partie Commence!";
+                // Optionally add a delay before the game timer starts
+                Invoke("StartGameTimer", 1); // 2 seconds delay
+            }
+        }
+        else if (timeRemaining > 0)
+        {
             timeRemaining -= Time.deltaTime;
             DisplayTime(timeRemaining);
         }
         else
         {
             Debug.Log("Time's Up!");
-            // Additional logic when the timer ends
+            // End game logic here
         }
     }
 
+    void StartGameTimer()
+    {
+        StartingGame.text = ""; 
+        TimerText.text = "";
+        // Start the game timer
+    }
     void DisplayTime(float timeToDisplay)
     {
         timeToDisplay += 1;
